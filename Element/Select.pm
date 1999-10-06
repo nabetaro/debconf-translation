b0ac4ee826cfd1a8da20d1fb78e5540fe872b3ba
Joey Hess <joeyh@debian.org>
1999-10-06 19:18:51 +0000

    Added.

---
+++ Element/Select.pm
@@ -0,0 +1,49 @@
+#!/usr/bin/perl -w
+
+=head1 NAME
+
+Debian::DebConf::Element::Select - Base select input element
+
+=cut
+
+=head1 DESCRIPTION
+
+This is a base Select input element.
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+package Debian::DebConf::Element::Select;
+use Debian::DebConf::Element::Base;
+use strict;
+use vars qw($AUTOLOAD);
+use vars qw(@ISA);
+@ISA=qw(Debian::DebConf::Element::Base);
+
+=head2 visible
+
+Select elements are not visible unless they have 2 or more choices to select from.
+
+=cut
+
+sub visible {
+	my $this=shift;
+
+	my @choices=$this->question->choices_split;
+
+	return '' if $#choices < 1;
+	
+	# Call parent class to deal with everything else.
+	return $this->SUPER::visible;
+}
+
+=head1 AUTHOR
+
+Joey Hess <joey@kitenet.net>
+
+=cut
+
+1
