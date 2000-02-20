8a2746340c3d4775264966faa3ce31459fb46d89
Joey Hess <joeyh@debian.org>
2000-02-20 09:18:54 +0000

    Added code for dynamically optimally sized columns, but it's not quite bug-free yet..

--- Element/Text/Select.pm
+++ Element/Text/Select.pm
@@ -18,6 +18,7 @@ This lets the user pick from a number of values, using a plain text interface.
 
 package Debian::DebConf::Element::Text::Select;
 use strict;
+use POSIX qw(ceil);
 use Debian::DebConf::Element::Select; # perlbug
 use base qw(Debian::DebConf::Element::Select);
 
@@ -88,6 +89,71 @@ sub expandabbrev {
 	return '';
 }
 
+=head2 printlist
+
+Pass first a reference to an array containing abbreviation info, then a list
+of all the choices the user has to choose from. Formats and displays the
+list, using multiple columns if necessary.
+
+=cut
+
+sub printlist {
+	my $this=shift;
+	my %abbrevs=%{(shift)};
+	my @choices=@_;
+	my $width=$this->frontend->screenwidth;
+
+	# Figure out the upper bound on the number of columns.
+	my $choice_min=length $choices[0];
+	map { $choice_min = length $_ if length $_ < $choice_min } @choices;
+	# (The 5 is 2 leading spaces + 1 char abbrev + 1 period + 1 space.)
+	my $max_cols=int($width / (5 + $choice_min)) - 1;
+	$max_cols = $#choices+1 if $max_cols > $#choices+1;
+
+	# Now work backwards, and find the maximum number of columns that
+	# really works. At the same time, come up with the width for each
+	# column.
+	my @col_width=();
+	my $total_width=0;
+	my $num_cols;
+COL:	for ($num_cols = $max_cols; $num_cols >= 0; $num_cols--) {
+		@col_width=();
+		foreach (my $choice=1; $choice < $#choices + 2; $choice++) {
+			my $choice_length=2 +
+				length($abbrevs{$choices[$choice - 1]}) + 2
+				+ length($choices[$choice - 1]);
+			my $current_col=ceil($choice / ceil(($#choices + 1) /
+						            ($num_cols + 1))) 
+             				- 1;
+			if (! defined $col_width[$current_col] ||
+			    $choice_length > $col_width[$current_col]) {
+				$col_width[$current_col]=$choice_length;
+				$total_width=0;
+				map { $total_width += $_ } @col_width;
+				next COL if $total_width > $width;
+			}
+		}
+		# If I get to here, the number of columns works.
+		last;
+	}
+
+	# Finally, generate and print the output.
+	my $line=0;
+	my $col=0;
+	my @output=();
+	foreach (my $choice=0; $choice <= $#choices; $choice++) {
+		$output[$line] .= "  $abbrevs{$choices[$choice]}. ".
+			$choices[$choice].
+			(' ' x ($col_width[$col] - length($choices[$choice]) - length($abbrevs{$choices[$choice]}) - 4));
+		if (++$line > ceil(($#choices + 1) / ($num_cols + 1))) {
+			$line=0;
+			$col++;
+		}	
+	}
+
+	map { print "$_\n" } @output;
+}
+
 sub show {
 	my $this=shift;
 
@@ -106,9 +172,7 @@ sub show {
 
 	# Print out the question.
 	$this->frontend->display($this->question->extended_description."\n");
-	foreach (@choices) {
-		$this->frontend->display_nowrap("\t$abbrevs{$_}. $_");
-	}
+	$this->printlist(\%abbrevs, @choices);
 	$this->frontend->display("\n");
 
 	# Prompt until a valid answer is entered.
