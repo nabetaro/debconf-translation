4bb083f211f4ecccd37e9d36e03392a5685adf05
Joey Hess <joeyh@debian.org>
2000-03-10 00:30:00 +0000

    So, earlier today, I had a basic screen being drawn, and basic windows you could position around the screen. Then I realized I was doing everything wrong, and started over with a more OO approach. Now I'm *almost* back to where I was 3 hours ago, I think -- though perl -e 'use lib ".";use FrontEnd::Slang; $f=Debian::DebConf::FrontEnd::Slang->new; sleep 10' prints absolute garbage now. :-)

--- Element/Slang.pm
+++ Element/Slang.pm
@@ -8,7 +8,7 @@ Debian::DebConf::Element::Slang - Base Slang Element.
 
 =head1 DESCRIPTION
 
-This is a base class for all the Slang Elements. These elements behave
+This is a base class for all the Slang Elements. These Elements behave
 differently than the elements used by other frontends.
 
 =cut
@@ -17,26 +17,44 @@ differently than the elements used by other frontends.
 
 =cut
 
-=head2 x
+=head2 xoffset
 
-The x coordinate the element may start at.
+The x offset of the element inside its container (if any).
 
 =cut
 
-=head2 y
+=head2 yoffset
 
-The y coordinate the element should occupy (elements are all 1 character
-tall).
+The y offset of the element inside its container (if any).
 
 =cut
 
 =head2 width
 
-The maximum width the element can take up on the screen.
+The width of the element.
+
+=cut
+
+=head2 height
+
+The height of the element.
 
 =cut
 
 =head2
+
+=head2 container
+
+The container the element is inside, if any.
+
+=cut
+
+=head2 contents
+
+If this element is a container, this property will contain a list of its
+contents.
+
+=cut
 
 =head1 METHODS
 
@@ -44,20 +62,118 @@ The maximum width the element can take up on the screen.
 
 package Debian::DebConf::Element::Slang;
 use strict;
+use Debian::DebConf::Element; # perlbug
 use base qw(Debian::DebConf::Element);
+
+sub new {
+        my $proto = shift;
+        my $class = ref($proto) || $proto;
+        my $self  = bless $proto->SUPER::new(@_), $class;
+
+	$self->contents([]);
+	return $self;
+}
+
+=head container
+
+Set/get the container this element is inside. Automatically updates the
+contents property of the container.
+
+=cut
+
+sub container {
+	my $this=shift;
+
+	return $this->{container} unless @_;
+	if ($this->{container}) {
+		my @contents=grep { $_ ne $this }
+			     @{$this->{container}->contents};
+		$this->{container}->contents(\@contents);
+	}
+	if (ref $_[0]) {
+		my @contents=(@{$_[0]->contents}, $this);
+		$_[0]->contents(\@contents);
+	}
+	return $this->{container}=$_[0];
+}
+
+=head resize
+
+This method is called whenever the layout of the screen needs to be
+changed. This method calls the function pointed to by the resize_hook
+property, if any. Typically, that function will look at the size of the
+element's container, and change the element's position and size
+appropriatly.
+
+This method causes any elements contained inside this one to be resized.
+
+=cut
+
+sub resize {
+	my $this=shift;
+
+	if (ref $this->resize_hook eq 'SUB') {
+		&{$this->resize_hook}(@_);
+	}
+
+	map { $_->resize(@_) } @{$this->contents};
+}
+
+=head2 x
+
+This returns the absolute x position of the element, taking into account
+that it may be inside a container.
+
+=cut
+
+sub x {
+	my $this=shift;
+
+	my $container=$this->container;
+	if ($container) {
+		return $this->xoffset + $this->container->x;
+	}
+	else {
+		return $this->xoffset;
+	}
+}
+
+=head2 y
+
+This returns the absolute y position of the element, taking into account
+that it may be inside a container.
+
+=cut
+
+sub y {
+	my $this=shift;
+
+	my $container=$this->container;
+	if ($container) {
+		return $this->yoffset + $this->container->y;
+	}
+	else {
+		return $this->yoffset;
+	}
+}
 
 =head2 display
 
 Draw the element on the screen. Its coordinates will have already been set.
-When an element is drawn, it should take care to overwrite everything from
-x,y to x+width,y.
 
 If a true value is passed, the element should be drawn highlighted (to
 indicate it is the active Element).
 
+This method should be overrided. All it does in this base element is
+display any elements contained inside this one.
+
 =cut
 
-sub display {}
+sub display {
+	my $this=shift;
+
+	map { $_->display(@_) } @{$this->contents};
+}
 
 =head2 activate
 
--- FrontEnd/Slang.pm
+++ FrontEnd/Slang.pm
@@ -19,6 +19,8 @@ This FrontEnd is a custom Slang UI for DebConf.
 package Debian::DebConf::FrontEnd::Slang;
 use strict;
 use Term::Slang;
+use Debian::DebConf::Element::Slang::Internal;
+use Debian::DebConf::Element::Slang::Internal::Window;
 use Debian::DebConf::FrontEnd::Tty; # perlbug
 use base qw(Debian::DebConf::FrontEnd::Tty);
 
@@ -27,27 +29,150 @@ sub new {
 	my $class = ref($proto) || $proto;
 	my $self  = bless $proto->SUPER::new(@_), $class;
 
-	$self->showdescs(1);
-	$self->sl(Term::Slang->new);
-	# Set up slang.
-	$self->sl->init_smg;
-	$self->sl->SLang_init_tty(-1,0,1);
-	$self->sl->SLkp_init;
-	
+	$self->startslang;
+	# Create the element that represents the entire screen.
+	$self->screen(Debian::DebConf::Element::Slang::Internal->new);
+	$self->screen->frontend($self);
+	$self->screen->xoffset(0);
+	$self->screen->yoffset(0);
+	$self->resize_hook(sub {
+		my $this=shift;
+
+		# Just resize to fit the passed parameters.
+		$this->width(shift);
+		$this->height(shift);
+	});
+	# Create the two main windows and put them in the screen.
+	my $qwin=Debian::DebConf::Element::Slang::Internal::Window->new;
+	$qwin->frontend($self);
+	$qwin->title("foo");
+	$qwin->container($self->screen);
+	$qwin->xoffset(2);
+	$qwin->yoffset(2);
+	$qwin->resize_hook(sub {
+		my $this=shift;
+
+		# Take up the top half of the screen.
+		$this->width($this->container->width - 4);
+		$this->height(($this->container->height - 4) / 2);
+	});
+	my $dwin=Debian::DebConf::Element::Slang::Internal::Window->new;
+	$dwin->frontend($self);
+	$dwin->title("Description");
+	$dwin->container($self->screen);
+	$self->resize;
 	return $self;
 }
 
-=head2 drawscreen
+=head2 startslang
 
-Draw the screen. The general layout is a window containing a scrollable form
-on the left that will later be populated by a variety of Elements. A narrower
-area on the right will hold the extended description of the Elements (if the
-showdescs property is set). Below these areas are some buttons that are used
-for controlling debconf.
+Initialize Slang. This includes setting up all the colors.
 
 =cut
 
-sub drawscreen {
+sub startslang {
+	my $this=shift;
+
+	$this->sl(Term::Slang->new);
+	$this->sl->init_smg;
+	$this->sl->SLang_init_tty(-1,0,1);
+	$this->sl->smg_init_smg;
+	$this->sl->SLkp_init;
+
+	my %colors;
+	my $color=0;
+	foreach (#color id	foreground	background
+		 [background =>	"white",	"blue"],
+		 [shadow =>	"gray",		"black"],
+		 [bar =>	"black",	"cyan"],
+		 [dialog =>	"black",	"white"],
+		) {
+		$colors{$_->[0]}=$color;
+		$this->sl->SLtt_set_color($color++, '', $_->[1], $_->[2]);
+	}
+	$this->color(\%colors);
+	$this->active(1);
+}
+
+=head2 resize
+
+We can use slang to get the screen size, and so we do, overriding the more
+cludgy way our parent handles this. All Elements are then resized, and the
+screen is redrawn.
+
+=cut
+
+sub resize {
+	my $this=shift;
+
+	if ($this->active) {
+		my ($height, $width)=$this->sl->SLtt_get_screen_size;
+		$this->screen->resize($this->screenheight($height - 1),
+				      $this->screenwidth($width));
+		$this->screen->display;
+		$this->sl->smg_refresh;
+	}
+}
+
+=head2 draw_titlebar
+
+Draw the titlebar at the top of the screen.
+
+=cut
+
+sub draw_titlebar {
+	my $this=shift;
+
+	$this->sl->smg_gotorc(0,0);
+	$this->sl->smg_set_color($this->color->{bar});
+	$this->sl->smg_erase_eol;
+	$this->sl->smg_gotorc(0,
+		($this->screenwidth - length($this->titlebar)) / 2);
+	$this->sl->smg_write_string($this->titlebar);
+}
+
+=head2 push_help
+
+Push help text onto the stack. This cases it to be displayed at the bottom
+of the screen. Pass in the text.
+
+=cut
+
+sub push_help {
+	my $this=shift;
+
+	push @{$this->helpstack}, @_;
+	$this->draw_helpbar;
+}
+
+=head2 pop_help
+
+Pops the current help text off the stack, causing the previous help text to
+be displayed. Returns the popped text.
+
+=cut
+
+sub pop_help {
+	my $this=shift;
+
+	my $ret=pop @{$this->helpstack};
+	$this->draw_helpbar;
+	return $ret;
+}
+
+=head2 draw_helpbar
+
+Draw the help bar at the bottom of the screen. The help bar displays the
+first element in the array referenced by the helpbar property.
+
+=cut
+
+sub draw_helpbar {
+	my $this=shift;
+
+	$this->sl->smg_gotorc($this->screenheight,0);
+	$this->sl->smg_set_color($this->color->{bar});
+	$this->sl->smg_write_nstring($this->helpstack->[0], $this->screenwidth);
 }
 
 =head2 go
@@ -103,6 +228,7 @@ sub shutdown {
 
 	$this->sl->SLang_reset_tty;
 	$this->sl->smg_reset_smg;
+	$this->active("");
 }
 
 =head1 AUTHOR
