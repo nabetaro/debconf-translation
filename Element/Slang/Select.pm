71275b481fd5cf061921234f8c7b04e93b76c1c7
Joey Hess <joeyh@debian.org>
2000-03-22 02:33:35 +0000

    hm, no need to make select elements _that_ wide..

--- Element/Slang/Select.pm
+++ Element/Slang/Select.pm
@@ -16,8 +16,7 @@ package Debian::DebConf::Element::Slang::Select;
 use strict;
 use Term::Stool::DropDown;
 use Debian::DebConf::Element::Select; # perlbug
-use base qw(Debian::DebConf::Element::Select
-	    Debian::DebConf::Element::Slang::String);
+use base qw(Debian::DebConf::Element::Select);
 
 sub makewidget {
 	my $this=shift;
@@ -41,8 +40,42 @@ sub makewidget {
 			cursor => $cursor,
 		),
 	));
-	# The widget prefers to be just wide enough for the list box.
-	$this->widget->preferred_width($this->widget->list->width + 4);
+	# The widget prefers to be just wide enough for the list box,
+	# plus one character. The one character makes it look better, IMHO.
+	$this->widget->preferred_width($this->widget->list->width + 5);
+}
+
+=head2 resize
+
+This is called when the widget is resized.
+
+Try to make the widget as wide as its preferred_width attrribute. If there's
+room for a widget that wide to fix on the same line as the description, do so.
+Otherwise, put the widget on the next line.
+
+=cut
+
+sub resize {
+	my $this=shift;
+	my $widget=$this->widget;
+	my $description=$widget->description;
+	my $maxwidth=$widget->container->width - 4;
+
+	if ($maxwidth > $widget->preferred_width + $description->width) {
+		$widget->sameline(1);
+		$widget->width($widget->preferred_width);
+		$widget->xoffset($description->width + 2);
+	}
+	elsif ($maxwidth > $widget->preferred_width) {
+		$widget->sameline(0);
+		$widget->width($widget->preferred_width);
+		$widget->xoffset(1);
+	}
+	else {
+		$widget->sameline(0);
+		$widget->width($maxwidth);
+		$widget->xoffset(1);
+	}
 }
 
 sub value {
