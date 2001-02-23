b04754d5e1723db2ceead21fbfc47f56f2d54eb7
Joey Hess <joeyh@debian.org>
2001-02-23 19:19:22 +0000

    Added beginfile and endfile hooks to formats.

--- Debconf/Format.pm
+++ Debconf/Format.pm
@@ -25,6 +25,11 @@ is a structure as required by Debconf::DbDriver::Cached. Note that the
 filehandle may contain multiple records, so it must be able to recognize an
 end-of-record delimiter of some kind and stop reading after it.
 
+=head2 beginfile(filehandle)
+
+Called at the beginning of each file that is written, before write() is
+called.
+
 =head2 write(filehandle, data, itemname)
 
 Format a record and and write it out to the filehandle. Should include an
@@ -32,6 +37,10 @@ end-of-record marker of some sort that can be recognized by the parse
 function.
 
 data is the same structure parse should return.
+
+=head2 endfile(filehandle)
+
+Called at the end of each file that is written.
 
 =head1 AUTHOR
 
