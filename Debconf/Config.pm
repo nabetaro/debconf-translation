3e33c9df8657e2217e971c3d318e63ec18e46c18
Colin Watson <cjwatson@debian.org>
2005-08-02 10:24:32 +0000

    * Add DEBCONF_DB_REPLACE environment variable with the same syntax as   DEBCONF_DB_OVERRIDE and DEBCONF_DB_FALLBACK, which bypasses all the   normal databases (thus avoiding locking them). Useful for local testing   or for running two concurrent debconf instances.

--- Debconf/Config.pm
+++ Debconf/Config.pm
@@ -137,9 +137,20 @@ sub load {
 		print STDERR "debconf: ".gettext("The Sigils and Smileys options in the config file are no longer used. Please remove them.")."\n";
 	}
 
+	# DEBCONF_DB_REPLACE bypasses the normal databases. We do still need
+	# to set up the normal databases anyway so that the template
+	# database is available, but we load them all read-only.
+	if (exists $ENV{DEBCONF_DB_REPLACE}) {
+		_env_to_driver($ENV{DEBCONF_DB_REPLACE}, name => "_ENV_REPLACE");
+		$config->{config} = "_ENV_REPLACE";
+	}
+
 	# Now read in each database driver, and set it up.
 	while (<DEBCONF_CONFIG>) {
 		my %config=(@defaults);
+		if (exists $ENV{DEBCONF_DB_REPLACE}) {
+			$config{readonly} = "true";
+		}
 		next unless _hashify($_, \%config);
 		eval {
 			Debconf::Db->makedriver(%config);
